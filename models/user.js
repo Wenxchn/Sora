518f0bf4dce5ad58ee6edcf75e8cf38a45cbd3ad
wenxchn <wenx.chn@gmail.com>
2020-11-29 06:18:54 -0500

    Delete and Edit Backend

--- models/user.js
+++ models/user.js
@@ -5,8 +5,8 @@ const mediaSchema = new Schema ({
     name: { type: String, required: true },
     author: { type: String, required: true },
     type: { type: String, required: true },
-    rating: {type: Number, min: 1, max: 10, required: true},
-    comment: {type: String, default: "" }
+    rating: { type: Number, min: 1, max: 10, required: true},
+    comment: { type: String, default: "" }
 })
 
 const userSchema = new Schema ({
--- routes/user.js
+++ routes/user.js
@@ -43,6 +43,34 @@ router.post('/add', isAuthenticated, async (req, res) => {
     }
 })
 
+router.post('/edit', isAuthenticated, async (req, res) => {
+    const { name, author, type, rating, comment, _id } = req.body
+    const username = req.session.username
+    const media = { name, author, type, rating, comment }
+    try {
+        const user = await User.findOne({ username })
+        user.mediaList.pull(_id)
+        user.mediaList.push(media)
+        user.save()
+        res.send('Media successfully edited')
+    } catch {
+        res.send('Failed to edit media')
+    }
+})
+
+router.post('/delete', isAuthenticated, async (req, res) => {
+    const { _id } = req.body
+    const username = req.session.username
+    try {
+        const user = await User.findOne({ username })
+        user.mediaList.pull(_id)
+        user.save()
+        res.send('Media successfully deleted')
+    } catch {
+        res.send('Failed to delete media')
+    }
+})
+
 router.post('/signup', async (req, res) => {
     const { username, password } = req.body
     try {
